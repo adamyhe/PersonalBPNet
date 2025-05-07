13a33e1f73f167c6f735973c6244ceb5d69b56d5
Adam Youlin He <ayh8@cornell.edu>
2025-05-06 20:54:22 -0400

    added a function for plotting genomic tracks

--- personal_bpnet/utils.py
+++ personal_bpnet/utils.py
@@ -5,6 +5,7 @@ Notably includes a data loader for loading chunked data to train CLIPNET.
 
 import random
 
+import matplotlib.pyplot as plt
 import numpy as np
 import pyfastx
 import torch
@@ -322,3 +323,33 @@ class WarmupScheduler(object):
             self.current_lr = lr
             for param_group in self.optimizer.param_groups:
                 param_group["lr"] = lr
+
+
+def plot_side(arr, ylim=[-2, 2.5], yticks=[0, 2], pic_name=None):
+    """
+    Adapted from APARENT code (Bogard et al. 2019)
+    """
+    assert arr.shape[0] % 2 == 0, "arr must have even length."
+    midpoint = int(arr.shape[0] / 2)
+    pl = arr[:midpoint]
+    mn = arr[midpoint:]
+    plt.bar(
+        range(pl.shape[0]),
+        pl,
+        width=-2,
+        color="r",
+    )
+    plt.bar(range(mn.shape[0]), -mn, width=-2, color="b")
+    axes = plt.gca()
+    axes.set_ylim(ylim)
+    axes.set_yticks(yticks)
+    axes.set_xticks([])
+    axes.spines[["right", "top", "bottom"]].set_visible(False)
+    plt.xlim(-0.5, pl.shape[0] - 0.5)
+    axes.tick_params(labelleft=False)
+
+    if pic_name is None:
+        plt.show()
+    else:
+        plt.savefig(pic_name, transparent=True)
+        plt.close()
